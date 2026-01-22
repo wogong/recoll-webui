55a1bf680fb2ead8080ffad07918317a402abf8b
wogong <hi@wogong.net>
2026-01-22 16:03:40 +0800

    feat: allow multiple config directories with -c option

--- webui-standalone.py
+++ webui-standalone.py
@@ -7,11 +7,15 @@ import webui
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--addr', default='127.0.0.1',help='address to bind to [127.0.0.1]')
 parser.add_argument('-p', '--port', default='8080', type=int, help='port to listen on [8080]')
-parser.add_argument('-c', '--config', default=None, type=str, help='configuration directory')
+parser.add_argument('-c', '--config', action='append', help='configuration directory (primary) or extra indices')
 args = parser.parse_args()
 
 if args.config:
-    os.environ["RECOLL_CONFDIR"] = args.config
+    # First -c is the primary config dir
+    os.environ["RECOLL_CONFDIR"] = args.config[0]
+    # Additional -c args are extra config dirs for searching multiple indices
+    if len(args.config) > 1:
+        os.environ["RECOLL_EXTRACONFDIRS"] = ' '.join(args.config[1:])
 
 # change to webui's directory and import
 if os.path.dirname(__file__) != "":
